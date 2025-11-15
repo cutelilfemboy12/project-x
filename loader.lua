d27c7a4bb45110d2b4bc3980da7a165a00f925cd
cutelilfemboy12 <ltsmash1336@gmail.com>
2025-11-16 00:20:58 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,5 @@
-
+if game.PlaceId == 18208629822 then -- raid grounds
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cutelilfemboy12/project-x/refs/heads/main/raid-grounds.lua"))()
+elseif game.PlaceId == 4888256398 or game.PlaceId == 15247475957 or game.PlaceId == 17227761001 then -- tournaemnt grounds
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/cutelilfemboy12/project-x/refs/heads/main/tournament-grounds.lua"))()
+end
